2be146aa266041218162fd83dda374c184a828d9
jolamola <mrdanielkim@gmail.com>
2015-05-12 18:01:37 -0700

    blah

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('slide').hover(
+		function(){
+			$(this).find('caption').slideDown(250);
+		},
+		function(){
+			$(this).find('caption').slideUp(250);
+		}
+	);
+});
